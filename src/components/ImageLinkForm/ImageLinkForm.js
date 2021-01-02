27250a29ba5934913988c6bc1936fac9c43e74c5
Sakibul Islam <sakibul962@gmail.com>
2021-01-02 11:48:50 +0600

    input type change

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,7 +4,7 @@ function ImageLinkForm({onInputChange, onButtonSubmit}) {
   return (
     <div className="patterns br3 pa4 shadow-3 dib">
       <div className='shadow-4'>
-        <input type="text" id="urlInput" className="f4 pa2 w-70" placeholder="Image url..." onChange={onInputChange}/>
+        <input type="search" id="urlInput" className="f4 pa2 w-70" placeholder="Image url..." onChange={onInputChange}/>
         <button className="f4 w-30 ph3 pv2 grow link bg-light-purple white dib" 
           onClick={(event) => {
               onButtonSubmit(); 
